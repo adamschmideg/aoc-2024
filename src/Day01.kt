e8d460bed0534314df946679f76e6f13f10dff08
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-12-02 22:18:26 +0100

    Day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numberPairs = input.map { line ->
+            // split line at consecutive spaces into two numbers and convert them to integers
+            val (first, second) = line.split("\\s+".toRegex()).map { it.toInt() }
+            Pair(first, second)
+        }
+        val firstColumnSorted = numberPairs.map { it.first }.sorted()
+        val secondColumnSorted = numberPairs.map { it.second }.sorted()
+        return firstColumnSorted.zip(secondColumnSorted).sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +17,13 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val resultEasy = part1(listOf("2\t3"))
+    check(resultEasy == 1, { "1 != $resultEasy" })
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val result = part1(testInput)
+    check(result == 11, { "11 != $result" })
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
