c2f211e32ce0f03927af6a4f625abaa8becfa638
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-12-03 20:13:04 +0100

    Day 3, part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,10 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val text = input[0]
+    fun evaluate(expression: String): Int {
         val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
         // Find not overlapping matches
-        val matches = regex.findAll(text)
+        val matches = regex.findAll(expression)
         var accumulator = 0
-        print("Matches: ${matches.count()}\nLast match: ${matches.lastOrNull()?.value}\n")
         for (match in matches) {
             val a = match.groups[1]!!.value.toInt()
             val b = match.groups[2]!!.value.toInt()
@@ -15,8 +13,15 @@ fun main() {
         return accumulator
     }
 
+    fun part1(input: List<String>): Int {
+        return evaluate(input.joinToString("\n"))
+    }
+
     fun part2(input: List<String>): Int {
-        return TODO()
+        val text = input.joinToString("\n")
+        val skipRegex = """don't\(\).*?do\(\)""".toRegex()
+        val cleanedText = skipRegex.replace(text, "")
+        return evaluate(cleanedText)
     }
 
     val testInput = readInput("Day03_test")
@@ -26,8 +31,9 @@ fun main() {
     check(result1 == 161, { "$result1" })
     part1(input).println()
 
-    val result2 = part2(testInput)
-    check(result2 == 4, { "4!= $result2" })
+    val testInput2 = listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+    val result2 = part2(testInput2)
+    check(result2 == 48, { "$result2" })
 
     part2(input).println()
 }
