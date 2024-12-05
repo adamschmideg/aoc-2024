2240ea2e41ea31422ea365cb49f46b06add15efa
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-12-05 16:07:36 +0100

    Day 5, part 1

---
+++ src/Day05.kt
@@ -0,0 +1,68 @@
+fun main() {
+    fun indexPageNumbers(pageNumbers: List<Int>): Map<Int, Int> {
+        val index = mutableMapOf<Int, Int>()
+        for (i in pageNumbers.indices) {
+            index[pageNumbers[i]] = i
+        }
+        return index
+    }
+
+    fun conformsToRules(orderingRules: Map<Int,Set<Int>>, pageNumbers: List<Int>): Boolean {
+        val index = indexPageNumbers(pageNumbers)
+        for (pre in orderingRules.keys) {
+            for (post in orderingRules.getOrDefault(pre, setOf())) {
+                if (pre in index && post in index && index[pre]!! > index[post]!!) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+
+    fun parseManual(input: List<String>): Pair<Map<Int, Set<Int>>, List<List<Int>>> {
+        val orderingRules = mutableMapOf<Int, Set<Int>>()
+        val pageNumbersList = mutableListOf<List<Int>>()
+
+        for (line in input) {
+            if (line.contains("|")) {
+                val (pre, post) = line.split("|").map { it.toInt() }
+                orderingRules[pre] = orderingRules.getOrDefault(pre, setOf()) + post
+            } else if (line.isBlank()) {
+                continue
+            } else {
+                val pageNumbers = line.split(",").map { it.toInt() }
+                pageNumbersList.add(pageNumbers)
+            }
+        }
+
+        return Pair(orderingRules, pageNumbersList)
+    }
+
+    fun getMiddleNumber(pageNumbers: List<Int>): Int {
+        return pageNumbers[pageNumbers.size / 2]
+    }
+
+    fun part1(input: List<String>): Int {
+        val middleNumbers = arrayListOf<Int>()
+        val (orderingRules, pageNumbersList) = parseManual(input)
+        for (pageNumbers in pageNumbersList) {
+            if (conformsToRules(orderingRules, pageNumbers)) {
+                middleNumbers.add(getMiddleNumber(pageNumbers))
+            }
+        }
+
+        return middleNumbers.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO("Your code here")
+    }
+
+    val testInput = readInput("Day05_test")
+    val input = readInput("Day05")
+
+    check(part1(testInput) == 143, { "${part1(testInput)}" })
+
+    println("Part 1: ${part1(input)}")
+}
