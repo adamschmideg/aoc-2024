8d85bc87a136d58f532e4091a6fb0f120f7edbd6
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-12-11 18:45:43 +0100

    Day 9, part 2, tests pass

--- src/Day09.kt
+++ src/Day09.kt
@@ -56,17 +56,79 @@ fun main() {
         val filesAndSpaces = parseMap(input[0])
         val arranged = rearrange(filesAndSpaces)
         val checksum = checksum(arranged)
-        if (input[0].length < 20) {
-            println("$input[0] ->")
-            println("  ${compact(filesAndSpaces)}")
-            println("  ${compact(arranged)}")
-            println("  $checksum")
-        }
         return checksum
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun rearrangeBetter(numbers: List<Int?>): List<Int?> {
+        val arr: Array<Int?> = numbers.toTypedArray()
+        var blockStart = arr.size
+        var blockEnd: Int
+        var currentNumber: Int = -1
+        var freeSpaceStart: Int
+        var freeSpaceEnd = 0
+        var hasFreeSpace: Boolean
+        while (true) {
+            // Find last block
+            blockEnd = blockStart
+            while (blockEnd >= 0) {
+                blockEnd--
+                if (arr[blockEnd] != null) {
+                    currentNumber = arr[blockEnd]!!
+                    break
+                }
+            }
+            if (blockEnd <= 0) {
+                break
+            }
+
+            // Find beginning of consecutive blocks
+            blockStart = blockEnd
+            while (blockStart > 0 && arr[blockStart] == currentNumber) {
+                blockStart--
+            }
+            blockStart++
+
+            // Find free space large enough to fit the block
+            hasFreeSpace = false
+            freeSpaceStart = -1
+            val blockSize = blockEnd - blockStart + 1
+            var freeSpaceSize = 0
+            while (freeSpaceStart < blockStart) {
+                freeSpaceStart++
+                if (arr[freeSpaceStart] == null) {
+                    // Find end of free space
+                    freeSpaceEnd = freeSpaceStart
+                    while (freeSpaceEnd < blockEnd && arr[freeSpaceEnd] == null) {
+                        freeSpaceEnd++
+                    }
+                    freeSpaceSize = freeSpaceEnd - freeSpaceStart
+                    if (freeSpaceSize >= blockSize) {
+                        hasFreeSpace = true
+                        break
+                    }
+                }
+            }
+            println("Block: $blockStart-$blockEnd, $blockSize, currentNumber: $currentNumber")
+            println("Free space: $freeSpaceStart-$freeSpaceEnd, $freeSpaceSize, hasFreeSpace: $hasFreeSpace")
+            println("Array before: ${compact(arr.toList())}")
+            // Move block to free space
+            if (hasFreeSpace) {
+                for (i in 0 until blockSize) {
+                    arr[freeSpaceStart + i] = currentNumber
+                    arr[blockStart + i] = null
+                }
+            }
+            println("Array after:  ${compact(arr.toList())}")
+            println()
+        }
+        return arr.toList()
+    }
+
+    fun part2(input: List<String>): BigInteger {
+        val filesAndSpaces = parseMap(input[0])
+        val arranged = rearrangeBetter(filesAndSpaces)
+        val checksum = checksum(arranged)
+        return checksum
     }
 
     val testInput = readInput("Day09_test")
@@ -83,8 +145,10 @@ fun main() {
 
     check(part1(testInput).toInt() == 1928, { "${part1(testInput)}" })
 
+    val testGot2 = part2(testInput).toInt()
+    check(testGot2 == 2858, { "$testGot2" })
     // check(part2(testInput) == 123, { "${part2(testInput)}" })
 
-    println("Part 1: ${part1(input)}")
+    // println("Part 1: ${part1(input)}")
     // println("Part 2: ${part2(input)}")
 }
