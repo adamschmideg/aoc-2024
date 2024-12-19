3a2870d0c0021822d6db0a33395f7a64499bf6e4
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-12-19 12:39:44 +0100

    Day 13, part 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,53 +1,57 @@
+import java.math.BigDecimal
+import java.math.BigInteger
 
 fun main() {
     val buttonRegex = Regex("""Button [AB]: X\+(\d+), Y\+(\d+)""")
     val prizeRegex = Regex("""Prize: X=(\d+), Y=(\d+)""")
 
-    fun parseLine(line: String, regex: Regex): Pair<Long, Long> {
+    fun parseLine(line: String, regex: Regex): Pair<BigInteger, BigInteger> {
         val match = regex.find(line) ?: throw IllegalArgumentException("Invalid input format: $line")
-        val x = match.groups[1]?.value?.toLong()!!
-        val y = match.groups[2]?.value?.toLong()!!
+        val x = match.groups[1]?.value?.toBigInteger()!!
+        val y = match.groups[2]?.value?.toBigInteger()!!
         return Pair(x, y)
     }
 
-    fun toLongOrZero(x: Double): Long {
-        val intValue = x.toLong()
-        if (x.compareTo(intValue) == 0) {
-            return intValue
+    fun divideOrZero(x: BigInteger, y: BigInteger): BigInteger {
+        if (x % y != BigInteger.ZERO) {
+            return BigInteger.ZERO
         }
-        return 0
+        return x / y
     }
-    fun solveEquation(p: Long, q: Long, r: Long, s: Long, x: Long, y: Long): Pair<Long, Long> {
-        val div: Double = (p * s - q * r).toDouble()
-        if (div == 0.0) {
-            return Pair(0, 0)
+
+    fun solveEquation(p: BigInteger, q: BigInteger, r: BigInteger, s: BigInteger, x: BigInteger, y: BigInteger): Pair<BigInteger, BigInteger> {
+        val div = p * s - q * r
+        if (div == BigInteger.ZERO) {
+            return Pair(BigInteger.ZERO, BigInteger.ZERO)
         }
-        val a = toLongOrZero((x * s - y * r) / div)
-        val b = toLongOrZero((x * q - y * p) / -div)
+        val a = divideOrZero(x * s - y * r, div)
+        val b = divideOrZero(x * q - y * p, -div)
         return Pair(a, b)
     }
 
-    fun countTokens(a: Long, b: Long): Long {
-        return a * 3 + b
+    fun countTokens(a: BigInteger, b: BigInteger): BigInteger {
+        return a * BigInteger.valueOf(3) + b
     }
 
-    fun part1(input: List<String>, addToX: Long = 0, addToY: Long = 0): Long {
-        var allTokens: Long = 0
+    fun part1(input: List<String>, addToX: BigInteger = BigInteger.ZERO, addToY: BigInteger = BigInteger.ZERO): BigInteger {
+        var allTokens: BigInteger = BigInteger.ZERO
         for (chunk in input.chunked(4)) {
             val (lineA, lineB, linePrize, _) = chunk
             val (p, q) = parseLine(lineA, buttonRegex)
             val (r, s) = parseLine(lineB, buttonRegex)
             val (x, y) = parseLine(linePrize, prizeRegex)
             val (a, b) = solveEquation(p, q, r, s, x + addToX, y + addToY)
-            val tokenCount = countTokens(a, b)
+            if (addToX == BigInteger.ZERO || (a > BigInteger.valueOf(100) && b > BigInteger.valueOf(100))) {
+                val tokenCount = countTokens(a, b)
+                allTokens += tokenCount
+            }
             // println("Parameters: $p, $q, $r, $s, $x, $y, $a, $b, $tokenCount")
-            allTokens += tokenCount
         }
         return allTokens
     }
 
-    fun part2(input: List<String>): Long {
-        val correction = 10000000000000
+    fun part2(input: List<String>): BigInteger {
+        val correction = BigInteger.valueOf(10000000000000)
         return part1(input, correction, correction)
     }
 
